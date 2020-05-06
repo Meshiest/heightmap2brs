fd61b1523cecc423401aa355f4faff178e889860
Meshiest <meek.mesh@gmail.com>
2020-05-06 17:07:23 -0500

    fixed cull, added tile and snap features, fixed large height differences not auto filling

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,33 @@ extern crate image;
 
 use brs::*;
 use brs::{chrono::DateTime, uuid::Uuid};
-use clap::{App, Arg};
+use clap::clap_app;
 use std::cmp::{max, min};
 
-fn image_from_file(file: String) -> image::RgbImage {
-    image::open(file).expect("No heightmap provided").to_rgb()
+type Pos = (i32, i32, i32);
+type Col = [u8; 3];
+
+#[derive(Debug)]
+struct GenOptions {
+    size: u32,
+    scale: u32,
+    cull: bool,
+    tile: bool,
+    snap: bool,
 }
 
-type Pos = (i32, i32, i32);
-type Col = (u8, u8, u8);
+// Open an image file
+fn image_from_file(file: String) -> image::RgbImage {
+    image::open(file).expect("Invalid image file").to_rgb()
+}
 
-fn ez_brick(size: u32, position: Pos, height: u32, color: Col) -> brs::Brick {
+// Brick creation helper
+fn ez_brick(size: u32, position: Pos, height: u32, color: Col, tile: bool) -> brs::Brick {
+    // require brick height to be even (gen doesn't allow odd height bricks)
+    let height = height + height % 2;
+
     brs::Brick {
-        asset_name_index: 0,
+        asset_name_index: tile.into(),
         size: (size, size, height),
         position: (
             position.0 * size as i32 * 2 + 5,
@@ -27,26 +41,33 @@ fn ez_brick(size: u32, position: Pos, height: u32, color: Col) -> brs::Brick {
         collision: true,
         visibility: true,
         material_index: 0,
-        color: ColorMode::Custom(Color::from_rgba(color.0, color.1, color.2, 255)),
-        owner_index: 0u32,
+        color: ColorMode::Custom(Color::from_rgba(color[0], color[1], color[2], 255)),
+        owner_index: 1u32,
     }
 }
 
+// given an array of bricks, create a save
 fn bricks_to_save(bricks: Vec<brs::Brick>) -> brs::WriteData {
     let author = User {
         id: Uuid::parse_str("a1b16aca-9627-4a16-a160-67fa9adbb7b6").unwrap(),
         name: String::from("Generator"),
     };
 
-    let brick_owners = vec![author.clone()];
+    let brick_owners = vec![User {
+        id: Uuid::parse_str("a1b16aca-9627-4a16-a160-67fa9adbb7b6").unwrap(),
+        name: String::from("Generator"),
+    }];
 
     WriteData {
         map: String::from("Plate"),
         author,
-        description: String::from("None"),
+        description: String::from("Save generated from heightmap file"),
         save_time: DateTime::from(std::time::SystemTime::now()),
         mods: vec![],
-        brick_assets: vec![String::from("PB_DefaultBrick")],
+        brick_assets: vec![
+            String::from("PB_DefaultBrick"),
+            String::from("PB_DefaultTile"),
+        ],
         colors: vec![],
         materials: vec![String::from("BMC_Plastic")],
         brick_owners,
@@ -57,9 +78,7 @@ fn bricks_to_save(bricks: Vec<brs::Brick>) -> brs::WriteData {
 fn gen_heightmap(
     heightmap_file: String,
     colormap_file: String,
-    size: u32,
-    scale: u32,
-    cull: bool,
+    options: GenOptions,
 ) -> brs::WriteData {
     println!("Reading files");
     let heightmap = image_from_file(heightmap_file);
@@ -69,6 +88,7 @@ fn gen_heightmap(
         image_from_file(colormap_file)
     };
 
+    // get the height of a pixel if it is in bounds
     let get_height = |x: i32, y: i32| {
         if x < 0 || x >= heightmap.width() as i32 || y < 0 || y >= heightmap.height() as i32 {
             0
@@ -77,12 +97,13 @@ fn gen_heightmap(
         }
     };
 
+    // get the color of a pixel
     let get_color = |x: i32, y: i32| colormap.get_pixel(x as u32, y as u32).0;
 
-    let neighbors = |x: i32, y: i32| vec![(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)];
+    // determine how tall a brick should be based on its neighbors
     let brick_height = |x: i32, y: i32| {
         let top = get_height(x, y);
-        let min = neighbors(x, y)
+        let min = vec![(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
             .into_iter()
             .map(|(x, y)| get_height(x, y))
             .min();
@@ -95,27 +116,43 @@ fn gen_heightmap(
     println!("Iterating pixels");
     for y in 0..heightmap.height() as i32 {
         for x in 0..heightmap.width() as i32 {
-            let raw_height = brick_height(x, y);
+            let raw_z = get_height(x, y);
 
-            if cull && raw_height == 0 {
+            // cull bricks with 0 height
+            if options.cull && raw_z == 0 {
                 continue;
             }
 
-            let raw_z = get_height(x, y);
-
-            let height = min(max(raw_height * scale as i32 / 2, 2), 500) as u32;
+            let raw_height = brick_height(x, y);
             let color = get_color(x, y);
-            let z = raw_z as i32 * scale as i32;
-            // println!("{} {} {} {}", x, y, z, height);
 
-            let brick = ez_brick(
-                size,
-                (x as i32, y as i32, z as i32),
-                (height + height % 2) as u32,
-                (color[0], color[1], color[2]),
-            );
+            let mut desired_height = max(raw_height * options.scale as i32 / 2, 2);
+            let mut z = raw_z as i32 * options.scale as i32;
 
-            bricks.push(brick);
+            if options.snap {
+                z += 4 - z % 4;
+                desired_height += 4 - desired_height % 4;
+            }
+
+            // until we've made enough bricks to fill the height
+            // add a brick with a max height of 500
+            while desired_height > 0 {
+                // pick height for this brick
+                let height = min(max(desired_height, 2), 500) as u32;
+
+                // make the brick
+                bricks.push(ez_brick(
+                    options.size,
+                    (x as i32, y as i32, z as i32),
+                    height,
+                    color,
+                    options.tile,
+                ));
+
+                // update Z and remaining height
+                desired_height -= height as i32;
+                z -= height as i32 * 2;
+            }
         }
     }
 
@@ -123,17 +160,20 @@ fn gen_heightmap(
 }
 
 fn main() {
-    let matches = App::new("Heightmap2BRS")
-        .version("0.2.0")
-        .author("cake")
-        .about("Converts png 2 brs file")
-        .arg(Arg::with_name("INPUT").required(true).index(1))
-        .arg(Arg::with_name("output").short("o").takes_value(true))
-        .arg(Arg::with_name("colormap").short("c").takes_value(true))
-        .arg(Arg::with_name("size").short("s").takes_value(true))
-        .arg(Arg::with_name("scale").short("x").takes_value(true))
-        .arg(Arg::with_name("cull").short("z"))
-        .get_matches();
+    let matches = clap_app!(heightmap =>
+        (version: "0.3.0")
+        (author: "github.com/Meshiest")
+        (about: "Converts heightmap png files to Brickadia save files")
+        (@arg INPUT: +required "Input heightmap PNG image")
+        (@arg output: -o --output +takes_value "Output BRS file")
+        (@arg colormap: -c --colormap +takes_value "Input colormap PNG image")
+        (@arg vertical: -v --vertical +takes_value "Vertical scale multiplier (default 1)")
+        (@arg size: -s --size +takes_value "Brick stud size (default 1)")
+        (@arg cull: --cull "Automatically remove bottom level bricks")
+        (@arg tile: --tile "Render bricks as tiles")
+        (@arg snap: --snap "Snap bricks to the brick grid")
+    )
+    .get_matches();
 
     let heightmap_file = matches.value_of("INPUT").unwrap().to_string();
     let colormap_file = matches.value_of("colormap").unwrap_or("").to_string();
@@ -150,14 +190,26 @@ fn main() {
         * 5;
 
     let scale = matches
-        .value_of("scale")
+        .value_of("vertical")
         .unwrap_or("1")
         .parse::<u32>()
         .expect("Scale must be integer");
 
     let cull = matches.is_present("cull");
+    let tile = matches.is_present("tile");
+    let snap = matches.is_present("snap");
 
-    let data = gen_heightmap(heightmap_file, colormap_file, size, scale, cull);
+    let data = gen_heightmap(
+        heightmap_file,
+        colormap_file,
+        GenOptions {
+            size,
+            scale,
+            cull,
+            tile,
+            snap,
+        },
+    );
 
     println!("Writing Save to {}", out_file);
     let mut write_dest = std::fs::File::create(out_file).unwrap();
